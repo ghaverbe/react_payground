077ba4515b70e4a56ccb2ace56b594381631c0cf
gh <gerd.haverbeck@posteo.de>
2024-08-12 15:22:12 +0000

    add function template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
 import { Foo } from "./components/ui/Foo";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { Fab } from "@mui/material";
+
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleInc = () => {
+    if (count < 10) setCount((count) => count + 1);
+  };
+
+  const handleDec = () => {
+    if (count > 0) setCount((count) => count - 1);
+  };
+
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <Button variant="contained">MU</Button>
+      {count}
+      <Box sx={{}}>
+        <Fab onClick={handleInc} color="primary" aria-label="add" size="small">
+          <AddIcon />
+        </Fab>
+        <Fab onClick={handleDec} color="primary" aria-label="add" size="small">
+          <RemoveIcon />
+        </Fab>
+      </Box>
+
       <Foo />
     </>
   );
--- src/components/ui/Foo.tsx
+++ src/components/ui/Foo.tsx
@@ -3,3 +3,18 @@ type FooProps = {};
 export const Foo: React.FC<FooProps> = () => {
   return <>FOO</>;
 };
+
+// ---
+interface KaaProps {
+  parameterName: string;
+}
+
+export function Kaa({ parameterName }: KaaProps): JSX.Element {
+  // Code der Komponente
+
+  return (
+    <>
+      <p>{parameterName}</p>
+    </>
+  );
+}
